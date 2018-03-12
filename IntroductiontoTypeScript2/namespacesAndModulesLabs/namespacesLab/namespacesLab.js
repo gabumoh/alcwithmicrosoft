0149268b3a4d0bef502538226861b11bef487f6c
gabumoh <gmaumoh@gmail.com>
2018-03-12 13:26:42 +0100

    couldn't fix the bug

--- IntroductiontoTypeScript2/namespacesAndModulesLabs/namespacesLab/namespacesLab.js
+++ IntroductiontoTypeScript2/namespacesAndModulesLabs/namespacesLab/namespacesLab.js
@@ -1,15 +1,15 @@
 var ArrayUtilities;
 (function (ArrayUtilities) {
     function reverseArray(array) {
-        return array.slice(0).reverse;
+        return array.slice(0).reverse();
     }
     ArrayUtilities.reverseArray = reverseArray;
     function lastItemOfArray(array) {
-        return array.slice(0).pop;
+        return array.slice(0).pop();
     }
     ArrayUtilities.lastItemOfArray = lastItemOfArray;
     function firstItemOfArray(array) {
-        return array.slice(0).shift;
+        return array.slice(0).shift();
     }
     ArrayUtilities.firstItemOfArray = firstItemOfArray;
     function concatenateArray(array1, array2) {
